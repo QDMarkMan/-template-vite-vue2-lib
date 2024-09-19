f3a8bcf73c29e493f85aafa1bf6caf8d74dbf112
tongfu.e <tongfu.e@xtalpi.com>
2024-09-19 14:58:47 +0800

    feat: update commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,5 +3,5 @@
  *  @Email [etongfu@outlook.com].
  *  @Date [2024-09-19 13:33:31].
  *-------------------------------------------------------------------------------------------- */
-const config = require('./config/commitlint/commitlint.cjs')
+const config = require('./config/commitlint/commitlint.config.cjs')
 module.exports = config
